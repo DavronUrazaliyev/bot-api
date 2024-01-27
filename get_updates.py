127378f61cdaa94d46757652621e5fe40721925a
Davron17 <dorazaliyev75@gmail.com>
2024-01-27 14:30:30 +0500

    updates

--- get_updates.py
+++ get_updates.py
@@ -1,35 +1,34 @@
-from settings import TOKEN
 import requests
+from settings import url
 from time import sleep
-from send_message import send_message
 
 
-
-def get_updates():
-    url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+def get_updates(url: str) -> dict:
+    endpoint = '/getUpdates'
+    url += endpoint
+    
     response = requests.get(url)
-
     if response.status_code == 200:
-        return response.json()['result']
-    else:
-        return False
+        return response.json()  
+    return response.status_code
+print(get_updates(url))
+def get_last_update(updates: dict) -> dict:
+    last_update = updates['result'][-1] 
 
+    return last_update
+def main():
+    last_update_id = -1
+    while True:
+        updates = get_updates(url)
+        last_update = get_last_update(updates)
 
-def get_last_update(updates: list[dict]) -> dict:
-    return updates[-1]
+        if last_update['update_id'] != last_update_id:
+            user = last_update['message']['from']
+            chat_id = last_update['message']['chat']['id']
+            text = last_update['message']['text']
+            print(text, 'from', user['first_name']," chat id",chat_id )
 
-
-last_update_id = -1
-while True:
-    updates = get_updates()
-    last_update = get_last_update(updates)
-
-
-    if last_update['update_id'] != last_update_id:
-        text = last_update['message']['text']
-        user = last_update['message']['from']
-
-        send_message(user['id'], text)
-        last_update_id = last_update['update_id']
-
-    sleep(0.5)
+            last_update_id = last_update['update_id']
+            
+        sleep(0.5)
+main()
